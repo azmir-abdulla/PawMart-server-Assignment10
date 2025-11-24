7a564bd1b4b201df6ec6ce9e548b753f1af98708
Azmir Abdulla <princeabdulla006@gmail.com>
2025-11-24 16:46:24 +0600

    1st api make done

--- index.js
+++ index.js
@@ -23,8 +23,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+
     await client.connect();
+    const db = client.db("kuttodb");
+    const recentlistsCollection = db.collection("recentlist");
+
+    app.get('/recentlist', async (req, res)=>{
+
+      const result = await recentlistsCollection.find().toArray();
+      console.log(result);
+
+
+
+      res.send(result)
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
